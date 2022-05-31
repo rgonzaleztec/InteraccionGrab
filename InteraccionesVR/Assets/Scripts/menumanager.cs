1ef7b561483dd6f03b22039e249c85e52ca2aad9
rgonzaleqtec <rojo@tec.ac.cr>
2022-05-31 07:39:21 -0600

    Saltos, pasillos, juego

--- InteraccionesVR/Assets/Scripts/menumanager.cs
+++ InteraccionesVR/Assets/Scripts/menumanager.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class menumanager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private GameObject saltoTecho;
+    [SerializeField]
+    private GameObject LlegadaEdi2;
+    [SerializeField]
+    private GameObject personaje;
   
     public void cargarEscena(int numescena)
     {
@@ -18,4 +24,14 @@ public class menumanager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void subirTecho()
+    {
+        personaje.transform.SetPositionAndRotation(saltoTecho.transform.position, saltoTecho.transform.rotation);
+    }
+
+    public void bajarTecho()
+        {
+            personaje.transform.SetPositionAndRotation(LlegadaEdi2.transform.position, LlegadaEdi2.transform.rotation);
+        }
 }
